633cce0907112c29336fd771838d6d0d30c3cda7
Micol <micolpa08@gmail.com>
2020-03-22 19:07:42 -0400

    vista detalle

--- frontend/views/habitacion/room-detail.php
+++ frontend/views/habitacion/room-detail.php
@@ -16,7 +16,53 @@ use yii\widgets\DetailView;
 		<h2 class="h2 font-weight-bold text-primary">Habitación #<?= $model->room_number ?> <a class='btn btn-pink mt-1' href="/habitacion/editar/?id=<?= $model->id ?>" title='Editar' style='float:right'>Editar</a></h2>
 		<hr>
 	</div>
-	<div class="col-md-12">
+	<div class="col-md-6">
+		<img src="/frontend/web/<?= $model->imagen_url ?>" width='100%'>
+		<div class="mt-2">
+		<div class="mt-4">
+			<span class="font-weight-bold h5 text-primary">Descripción</span>
+			<p class="h4 font-weight-light"><?= $model->description; ?></p>
+		</div>
+		</div>
+	</div>
+	<div class="col-md-6">
+		
+		<div>
+			<span class="font-weight-bold h5 text-primary">Tipo</span>
+			<p class="h4 font-weight-light"><?= $model->ocean_view==0?$model->type->name:$model->type->name." Ocean View" ?></p>
+		</div>
+		<div class="mt-4">
+			<span class="font-weight-bold h5 text-primary">Status</span>
+			<p class="h4 font-weight-light"><?= $model->status->name; ?></p>
+		</div>
+		<div class="mt-4">
+			<span class="font-weight-bold h5 text-primary">Capacidad de PAX</span>
+			<p class="h4 font-weight-light"><?= $model->people_capacity; ?> Persona(s)</p>
+		</div>
+		<div class="mt-4">
+			<span class="font-weight-bold h5 text-primary">Cant. de Camas</span>
+			<p class="h4 font-weight-light"><?= $model->bed; ?> Cama(s)</p>
+		</div>
+		<div class="mt-4">
+			<span class="font-weight-bold h5 text-primary">Descripción de Camas</span>
+			<p class="h4 font-weight-light"><?= $model->bed_description; ?></p>
+		</div>
+		<div class="mt-4">
+			<span class="font-weight-bold h5 text-primary">Vistas</span><br><br>
+			<?php if ($model->ocean_view == 1): ?>
+				<span class="h6 font-weight-light rounded border border-primary pt-1 pb-1 pl-2 pr-2 mr-2"><i class="fas fa-water text-primary"></i> Ocean View</span>
+			<?php endif ?>
+			<?php if ($model->pool_view == 1): ?>
+			<span class="h6 font-weight-light rounded border border-success pt-1 pb-1 pl-2 pr-2 mr-2"><i class="fas fa-swimmer text-success"></i> Pool View</span>	
+			<?php endif ?>
+			<?php if ($model->street_view == 1): ?>
+			<span class="h6 font-weight-light rounded pt-1 pb-1 pl-2 pr-2" style="border:1px solid #f67575"><i class="fas fa-city text-pink"></i> Street View</span>	
+			<?php endif ?>
+		</div>
+
+	</div>
+
+	<div class="col-md-6" style="display: none">
 		<?= DetailView::widget([
                 'model' => $model,
                 'attributes' => [
