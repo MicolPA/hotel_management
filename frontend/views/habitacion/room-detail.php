2670b2bdf20f25c9db026fe7159dd1d27b449ace
Micol <micolpa08@gmail.com>
2020-03-22 17:56:01 -0400

    imagen url

--- frontend/views/habitacion/room-detail.php
+++ frontend/views/habitacion/room-detail.php
@@ -107,6 +107,7 @@ use yii\widgets\DetailView;
 	function getImagen(){
 		url = $("#img").val();
 		// url = "/frontend/web/"+url;
+		url "/"+url;
 		console.log(url);
 		swal({
             title: "",
